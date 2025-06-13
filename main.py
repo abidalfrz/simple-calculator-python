98f5d4ea30d1a1dbd3620841418fd0d050afb167
abidalfrz <abidalfaridzi18@gmail.com>
2025-06-14 00:08:52 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+# Simple Calculator Program
+
+def add(x, y):
+    """Return the sum of x and y."""
+    return x + y
+
+def sub(x, y):
+    """Return the difference of x and y."""
+    return x - y
+
+def main():
+    print("-"*10,"Simple Calculator", "-"*10)
+    print("Select option:")
+    print("1. Addition")
+    print("2. Subtraction")
+    choice = input("Enter choice (1/2): ")
+    if choice in ('1', '2'):
+        a = int(input("Enter first number: "))
+        b = int(input("Enter second number: "))
+
+        if choice == '1':
+            print(f"Result = {add(a, b)}")
+        elif choice == '2':
+            print(f"Result = {sub(a, b)}")
+    else:
+        print("Invalid Input!")
+
+
+if __name__ == "__main__":
+    main()
